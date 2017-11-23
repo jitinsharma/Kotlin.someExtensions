16c4e1dd627d6c7e38dc5c4746a1608bd3fb33a4
Jitin Sharma <jitin.sharma@amadeus.com>
2017-11-23 20:49:01 +0530

    bitmap functions

--- Extensions.kt
+++ Extensions.kt
@@ -72,3 +72,20 @@ fun String.convertStringToDate(simpleDateFormatPattern: String): Date? {
     }
     return value
 }
+
+/**
+ * Convert byte array to bitmap
+ */
+fun ByteArray.convertBytesToBitmap(): Bitmap =
+        BitmapFactory.decodeByteArray(this, 0, size)
+
+/**
+ * Convert bitmap to a byte array
+ */
+fun Bitmap.convertBitmapToBytes(): ByteArray {
+    val bytes: ByteArray
+    val stream = ByteArrayOutputStream()
+    this.compress(Bitmap.CompressFormat.PNG, 0, stream)
+    bytes = stream.toByteArray()
+    return bytes
+}
