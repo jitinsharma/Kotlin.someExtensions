acf1fc66b4fecad4377304d774de1638ce8be412
Jitin Sharma <jitins2892@gmail.com>
2017-12-02 11:38:54 +0530

    feat: split up extensions

---
+++ DateExtensions.kt
@@ -0,0 +1,33 @@
+package io.github.jitinsharma.kotlinsomeextensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Convert a given date to milliseconds
+ */
+fun Date.toMillis() : Long {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    return calendar.timeInMillis
+}
+
+/**
+ * Checks if dates are same
+ */
+fun Date.isSame(to : Date) : Boolean {
+    val sdf = SimpleDateFormat("yyyMMdd", Locale.getDefault())
+    return sdf.format(this) == sdf.format(to)
+}
+
+/**
+ * Converts raw string to date object using [SimpleDateFormat]
+ */
+fun String.convertStringToDate(simpleDateFormatPattern: String): Date? {
+    val simpleDateFormat = SimpleDateFormat(simpleDateFormatPattern, Locale.getDefault())
+    var value: Date? = null
+    justTry {
+        value = simpleDateFormat.parse(this)
+    }
+    return value
+}
