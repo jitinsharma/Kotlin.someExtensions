22714849a49138fb3bb500e6e053a157e842c911
Jitin Sharma <jitins2892@gmail.com>
2017-12-06 18:30:27 +0530

    feat: Added function blocks for thread execution

--- Blocks.kt
+++ Blocks.kt
@@ -1,5 +1,8 @@
 package io.github.jitinsharma.kotlinsomeextensions
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Build
 
 /**
@@ -10,17 +13,34 @@ inline fun <T> justTry(block: () -> T) = try { block() } catch (e: Throwable) {}
 /**
  * App's debug mode
  */
-inline fun debugMode(body : () -> Unit) {
+inline fun debugMode(block : () -> Unit) {
     if (BuildConfig.DEBUG) {
-        body()
+        block()
     }
 }
 
 /**
  * For functionality supported above API 21 (Eg. Material design stuff)
  */
-private inline fun lollipopAndAbove(body : () -> Unit) {
+inline fun lollipopAndAbove(block : () -> Unit) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        body()
+        block()
+    }
+}
+
+/**
+ * Execute block of code if network is available
+ */
+@SuppressLint("MissingPermission")
+fun Context.withConnection(block: () -> Unit) {
+    val connectivityManager = this
+            .getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+    connectivityManager?.let {
+        val netInfo = it.activeNetworkInfo
+        netInfo?.let {
+            if (netInfo.isConnected) {
+                block()
+            }
+        }
     }
 }
